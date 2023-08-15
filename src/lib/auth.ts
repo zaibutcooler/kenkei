cd3d1db3835378013e631b1eb314fb0eb6262239
Zai <zaiyellyintaung@gmail.com>
2023-08-15 15:57:29 +0630

    connecting db and setting next auth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,1 +1,20 @@
-//auth options here
+import { NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { UpstashRedisAdapter } from "@next-auth/upstash-redis-adapter";
+import { db } from "./db";
+
+export const authOptions: NextAuthOptions = {
+  adapter: UpstashRedisAdapter(db),
+  session: {
+    strategy: "jwt",
+  },
+  pages: {
+    signIn: "/login",
+  },
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+};
--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,1 +1,6 @@
-//connect Database
+import { Redis } from "@upstash/redis";
+
+export const db = new Redis({
+  url: process.env.UPSTASH_REDIS_REST_URL as string,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN as string,
+});
